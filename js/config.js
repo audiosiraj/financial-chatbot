670a2edacfacad432631be7c203be00c9381921e
SHAIK CHIRAJUDDIN <sirajaudio0786@gmail.com>
2025-11-09 19:15:25 +0530

    Update API key in config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // API Configuration
-const API_KEY = "AIzaSyBSJF4yypmcxyCPzDpN8pdu8pXJd6sqDD8";
+const API_KEY = "YOUR GEMINI API KEY!";
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-09-2025:generateContent?key=${API_KEY}`;
 
 // System Prompt Template
